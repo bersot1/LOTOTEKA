898ee4d4954ea9df70fc15fb02b09a0a1d5af118
Lucas Bersot <lucasbmarinho9@gmail.com>
2020-03-21 00:01:34 -0300

    feat: play any old game, new query sqlite

--- src/app/pages/page-draw-numbers/page-draw-numbers.page.ts
+++ src/app/pages/page-draw-numbers/page-draw-numbers.page.ts
@@ -4,7 +4,7 @@ import { DatabaseService } from 'src/app/providers/database.service';
 import { GameService } from '../../providers/game.service'
 import * as Tesseract from 'tesseract.js'
 import { Camera, PictureSourceType } from '@ionic-native/camera/ngx';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx'
+import * as moment from 'moment';
 
 
 @Component({
@@ -19,17 +19,17 @@ export class PageDrawNumbersPage implements OnInit {
   public contentIsLoad: boolean = false;
   public isRandomNumbers: boolean = true;
 
+  public howManyGame: number = 1;
   public week: any;
   public myGame: any;
   public numersRandom: any[] = [];
   public loading: any;
-  public objGame: any = {
-    week: '',
-    numbers: [],
-  }
+  public objGame: any[] = [];
 
   public selectedImage: string;
   public imageText: string;
+  public myGames: any;
+  public dateChosen: any;
 
 
 
@@ -40,8 +40,7 @@ export class PageDrawNumbersPage implements OnInit {
     public navCtrl: NavController,
     private camera: Camera,
     private actionSheetCtrl: ActionSheetController,
-    private loadingCtrl: LoadingController,
-    private barcodeScanner: BarcodeScanner
+    private loadingCtrl: LoadingController
   ) { }
 
   ngOnInit() {
@@ -51,44 +50,88 @@ export class PageDrawNumbersPage implements OnInit {
 
   public contentLoad() {
     this.startDbSQlite();
-    this.contentIsLoad = true;
+    this.getAllMeusJogos();
+    
   }
 
   public generateNumbers() {
-    this.objGame.numbers = [];
-    let validatorNumberEquals = 0;
-    let i = 0;
-
+    debugger
     if (this.week === undefined || this.week === null) {
       this.alerts('PickAWeek')
     }
     else {
+      // this.objGame.numbers = [];
+      let validatorNumberEquals = 0;
+      let i = 0;
 
-      this.formatDate(this.week.split('T'));
-
-      while (i < 15) {
-        validatorNumberEquals = 0;
-        let min = Math.ceil(1);
-        let max = Math.floor(25);
-        let result = Math.floor(Math.random() * (max - min)) + min;
-        if (i === 0) {
-          this.objGame.numbers.push(result);
-          i++;
-        } else {
-          this.objGame.numbers.forEach(e => {
-            if (e === result) {
-              validatorNumberEquals = + 1
-            }
-          });
-
-          if (validatorNumberEquals === 0) {
-            this.objGame.numbers.push(result)
-            i++;
-          }
+     
+      // let games = 1;
+      debugger
+      for (let index = 0; index < this.howManyGame; index++) {
+        this.objGame[index] = {
+          id: index+1,
+          Week: moment(this.week).format('DD-MM-YYYY'),
+          NumberOfGame: ''
         }
       }
+
+
+      for (let index = 0; index < this.howManyGame; index++) {
+        i = 0;
+        let arrayAuxGame : any[] = []
+
+        while (i < 15) {
+          validatorNumberEquals = 0;
+          let min = Math.ceil(1);
+          let max = Math.floor(25);
+          let result = Math.floor(Math.random() * (max - min)) + min;
+          if (i === 0) {
+            arrayAuxGame.push(result);
+            i++;
+          } else {
+            arrayAuxGame.forEach(e => {
+              if (e === result) {
+                validatorNumberEquals = + 1
+              }
+            });
+
+            if (validatorNumberEquals === 0) {
+              arrayAuxGame.push(result)
+              i++
+            }
+          }
+          arrayAuxGame.sort(this.compararNumeros)
+        }
+
+       
+
+        if (index > 0) {
+          let auxExistedGame = 0
+            this.objGame.forEach(e => {
+              if (e.NumberOfGame === arrayAuxGame) {
+                auxExistedGame += 1
+              }
+            });
+
+            if (auxExistedGame > 0) {
+              index--; 
+            }else {
+              this.objGame[index].NumberOfGame = arrayAuxGame;
+            }
+        }else {
+          this.objGame[index].NumberOfGame = arrayAuxGame;
+        }
+      }
+      
+      
     }
-    this.drawNumbers = true;
+    console.log(this.objGame);
+    
+    this.drawNumbers = !this.drawNumbers;
+  }
+
+  public selectOlderGame() {
+    this.drawNumbers = !this.drawNumbers;
   }
 
   public async alerts(param: string) {
@@ -105,34 +148,59 @@ export class PageDrawNumbersPage implements OnInit {
     }
   }
 
-  public formatDate(date: string) {
-    let aux = date[0].split('-')
-    this.objGame.week = aux[2] + '/' + aux[1] + '/' + aux[0];
-
-  }
-
   public startDbSQlite() {
     this.dbService.initDB();
   }
 
   public async insertGame() {
+    debugger
+    
+      let ConfirmSaveGame = await this.alertCtrl.create({
+        mode: 'ios',
+        header: 'Confirmar',
+        message: 'Deseja realmente salvar este jogo?',
+        buttons: [{
+          text: 'Sim', handler: () => {
+            this.objGame.forEach(e => {
+              e.Week = this.week
+            });
+            this.gameProvider.insert(this.objGame)
+            this.objGame = [];
+            this.drawNumbers = false;
+          }
+        },
+        { text: 'Cancelar' }]
+      });
 
-    let ConfirmSaveGame = await this.alertCtrl.create({
-      mode: 'ios',
-      header: 'Confirmar',
-      message: 'Deseja realmente salvar este jogo?',
-      buttons: [{
-        text: 'Sim', handler: () => {
-          this.gameProvider.insert(this.objGame.week, this.objGame.numbers)
-          this.objGame = [];
-          this.drawNumbers = false;
-        }
-      },
-      { text: 'Cancelar' }]
-    });
+      await ConfirmSaveGame.present();
+    
 
-    await ConfirmSaveGame.present();
 
+
+  }
+
+
+  public getAllMeusJogos() {
+    debugger
+    this.gameProvider.getAllOrderByDate().then(data => {
+      this.myGames = data
+      console.log('mygames',this.myGames); 
+     
+      this.contentIsLoad = true;
+
+    })
+  }
+
+  public getNumersOfGameByDateChosen(){
+    console.log(this.dateChosen);
+    this.gameProvider.getAllGamesByDate(this.dateChosen).then(data => {
+     
+      this.objGame.push(data);
+     
+      console.log('getNumbersOfGameByDateChosen',this.objGame);
+      
+    })
+    
   }
 
   public async selectSource() {
@@ -193,15 +261,6 @@ export class PageDrawNumbersPage implements OnInit {
     await load.present();
   }
 
-  public readBarCode() {
-    this.barcodeScanner.scan().then((data: any) => {
-      console.log('barcodeScanner', data);
-
-    }).catch(er => {
-      console.log('error barcode', er);
-
-    })
-  }
 
   public inputmMyGame() {
     this.isInputMyGame = !this.isInputMyGame
@@ -220,10 +279,24 @@ export class PageDrawNumbersPage implements OnInit {
   }
 
   public maskGame(input) {
-    
 
-    console.log(input.detail.value);
 
+  }
+
+  public compararNumeros(a, b) {
+    return a - b;
+  }
+
+
+  public async alert(eHeader, eMessage) {
+    let alert = await this.alertCtrl.create({
+      mode: 'ios',
+      header: eHeader,
+      message: eMessage,
+      buttons: [{ text: 'Ok' }]
+    })
+
+    await alert.present();
   }
 
 }
--- src/app/providers/game.service.ts
+++ src/app/providers/game.service.ts
@@ -10,43 +10,97 @@ export class GameService {
   constructor(private dbProvider: DatabaseService) { }
 
 
-    public insert(week: any, numbers: any) {
-        debugger
-        let data = [week, JSON.stringify(numbers)]
-        return this.dbProvider.initDB()
-            .then((db: SQLiteObject) => {
-                let sql = 'insert into Games (Week,NumberOfGame) VALUES (?,?)'
-                return db.executeSql(sql, data).then(() => {
-                  console.log('inserido com sucesso');
-                  
-                })
-                .catch((e) => { console.log('error ao inserir', e);
-                 })
-            }).catch((e) => {
-                console.log('Error no initdb', e);
-                
-            })
-    }
+  public insert(obj) {
+  
+    for (let index = 0; index < obj.length; index++) {
+      let data = [obj[index].Week, JSON.stringify(obj[index].NumberOfGame)]
+      var result = this.dbProvider.initDB()
+      .then((db: SQLiteObject) => {
+        let sql = 'insert into Games (Week,NumberOfGame) VALUES (?,?)'
+        return db.executeSql(sql, data).then(() => {
+          console.log('inserido com sucesso');
 
-    public getAll() {
-      return this.dbProvider.initDB()
-        .then((db: SQLiteObject) => {
-          return db.executeSql('SELECT * FROM Games', [])
-            .then((data) => {
-              console.log('then select all');
-              
-              let games: any[] = [];
-              for (let i = 0; i < data.rows.length; i++) {
-                
-                let item = data.rows.item(i);
-                console.log('item for', item);
-                
-                games.push(item);
-                return games;
-        }
-            })
-            .catch((e) => console.error(e));
         })
-        .catch((e) => console.error(e));
+          .catch((e) => {
+            console.log('error ao inserir', e);
+          })
+      }).catch((e) => {
+        console.log('Error no initdb', e);
+
+      })
     }
+    return result
+  }
+
+  public getAllOrderByDate() {
+    return this.dbProvider.initDB()
+      .then((db: SQLiteObject) => {
+        return db.executeSql('SELECT Week FROM Games GROUP BY Week ', [])
+          .then((data) => {
+            debugger
+            console.log('then select all');
+
+            let games: any[] = [];
+            for (let i = 0; i < data.rows.length; i++) {
+
+              let item = data.rows.item(i);
+              console.log('item for', item);
+
+              games.push(item);
+
+            }
+            return games;
+          })
+          .catch((e) => console.error(e));
+      })
+      .catch((e) => console.error(e));
+  }
+
+
+  public getAll() {
+    return this.dbProvider.initDB()
+      .then((db: SQLiteObject) => {
+        return db.executeSql('SELECT * FROM Games', [])
+          .then((data) => {
+            debugger
+            console.log('then select all');
+
+            let games: any[] = [];
+            for (let i = 0; i < data.rows.length; i++) {
+
+              let item = data.rows.item(i);
+              console.log('item for', item);
+
+              games.push(item);
+
+            }
+            return games;
+          })
+          .catch((e) => console.error(e));
+      })
+      .catch((e) => console.error(e));
+  }
+  public getAllGamesByDate(date) {
+    return this.dbProvider.initDB()
+      .then((db: SQLiteObject) => {
+        return db.executeSql('SELECT * FROM Games G WHERE G.Week = ?', [date])
+          .then((data) => {
+            debugger
+            console.log('then select all games by date');
+
+            let games: any[] = [];
+            for (let i = 0; i < data.rows.length; i++) {
+
+              let item = data.rows.item(i);
+              console.log('item for by date', item);
+
+              games.push(item);
+
+            }
+            return games;
+          })
+          .catch((e) => console.error(e));
+      })
+      .catch((e) => console.error(e));
+  }
 }
