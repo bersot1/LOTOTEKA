90de3a2a1a0bf0da55a7fbdd9dae8130b04e84d3
Lucas Bersot <lucasbmarinho9@gmail.com>
2020-04-13 07:50:59 -0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,18 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { SQLite } from '@ionic-native/sqlite/ngx'
 
-import { Camera } from '@ionic-native/camera/ngx';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx'
-
-import { BrMaskerModule } from 'br-mask';
 import { ModalImportGamePageModule } from './pages/modal-import-game/modal-import-game.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,BrMaskerModule, ModalImportGamePageModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ModalImportGamePageModule],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     SQLite,
-    Camera,
-    BarcodeScanner
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
